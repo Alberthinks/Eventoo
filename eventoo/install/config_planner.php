64ac846e1eb342b1958ceeeebe14bb53d9ccb44a
Alberthinks <117525757+Alberthinks@users.noreply.github.com>
2023-07-29 12:34:04 +0200

    Delete eventoo directory

--- eventoo/install/config_planner.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "eventooRootUser";
-$password = "QnBWQzlN-vVko9Egryb5b4&k1b4hghb2bj1jkj4$";
-$dbname = "eventoo_planner";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
